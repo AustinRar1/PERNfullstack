4af3f2e1b5baf26f19c44d7e68664bdd1c600290
Austin Rardin <ajeflaunch@gmail.com>
2018-07-03 14:42:12 -0600

    addedgit

--- src/helpers/enviroment.js
+++ src/helpers/enviroment.js
@@ -4,7 +4,8 @@ switch (window.location.hostname) {
     case 'localhost':
             APIURL = 'http://localhost:3000'
         break;
-
+    case 'ajr-voidstartclient.herokuapp.com':
+    APIURL = 'https://ajr-voidstart.herokuapp.com/'
     default:
         break;
 }
